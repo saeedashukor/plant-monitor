d4eff90516da78e6710b3818526e3ee15bed745e
Saeeda <saeedashukor@outlook.com>
2025-07-18 00:08:45 +0100

    fix: validate request body

--- src/pages/api/logs.ts
+++ src/pages/api/logs.ts
@@ -1,17 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "~/server/db";
+import { z } from "zod";
+
+const MoistureSchema = z.object({
+  moisture: z.number(),
+});
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse){
     if (req.method !== "POST") {
         return res.status(405).json({ error: "Method not allowed" });
     }
 
-    const { moisture } = req.body;
+    const parse = MoistureSchema.safeParse(req.body);
 
-    if (typeof moisture !== "number") {
-        return res.status(400).json({error: "Moisture must be a number"});
+    if (!parse.success) {
+        return res.status(400).json({error: "Invalid body"});
     }
 
+    const { moisture } = parse.data;
     await db.moistureLog.create({
         data: { moisture },
     });
